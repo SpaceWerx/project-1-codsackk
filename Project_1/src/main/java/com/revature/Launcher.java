627ebc26401083d8e2ca1d4041f408a7045ba780
Cody Taylor Jenkins <codytaylorjenkins@Codys-MacBook-Pro.local>
2022-05-23 16:19:29 -0500

    JAVALIN ADDED

--- Project_1/src/main/java/com/revature/Launcher.java
+++ Project_1/src/main/java/com/revature/Launcher.java
@@ -1,7 +1,11 @@
 package com.revature;
 
+import com.revature.controllers.UserController;
 //import java.util.Scanner;
 import com.revature.utilities.ConnectionFactory;
+
+import io.javalin.Javalin;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -9,7 +13,8 @@ public class Launcher {
 	
 	
 	public static void main(String[] args) throws SQLException {
-		
+			
+			UserController uc = new UserController();
 			
 			//Testing Database Connectivity - just testing whether our Connection (from ConnectionFactory) is successful
 			try(Connection conn = ConnectionFactory.getConnection()){
@@ -24,7 +29,18 @@ public class Launcher {
 			//Menu menu = new Menu();
 				
 			//menu.printMenu();
+			
+			Javalin app = Javalin.create(
+				config-> {
+					config.enableCorsForAllOrigins();
+				}
+					).start(3000);
 		
+			app.get("/user", uc.getUsersHandler);
+			
+			app.post("/user", uc.insertUsersHandler);
+			
+			//app.post("/user", null);
 		}
 	}
 
---
+++ Project_1/src/main/java/com/revature/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.revature.controllers;
+
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.revature.Service.User_Services;
+import com.revature.models.Users;
+
+import io.javalin.http.Handler;
+
+public class UserController {
+	
+	User_Services us = new User_Services();
+
+	public Handler getUsersHandler = (ctx) -> {
+		
+		List<Users>	allUsers = us.getUsers();
+		
+		Gson gson = new Gson();
+		
+		String JSONObject = gson.toJson(allUsers);
+		
+		ctx.result(JSONObject);
+		ctx.status(200);
+		
+	};
+	
+	
+	public Handler insertUsersHandler = (ctx) -> {
+		
+		String body = ctx.body();
+		
+		Gson gson = new Gson();
+		
+		Users user = gson.fromJson(body, Users.class);
+		
+		us.insertUser(user);
+		
+		ctx.result("User added successfully!");
+		ctx.status(201);
+		
+	};
+
+}
